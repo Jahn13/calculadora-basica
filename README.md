942aecf40c76ed6f4640c09f1db00aef7f4264a1
Cesar Puac <jmangochango642@gmail.com>
2023-04-13 10:20:55 -0600

    Comentando el codigo y subiendo el readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculadora Basica con el DOM
+
+## Mi primera calculadora Utilizando el DOM y eventos.
+
+![image text](./img/captura-calculadora.png)
--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const limpiar = document.querySelector("#clean");
 const igual = document.querySelector("#r");
 let resultado = 0;
 
+//Escuchará el click de los botones de numeros y los operadores
 const btn = document.querySelectorAll("#num");
 for(i=0;i<btn.length;i++){
     btn[i].addEventListener("click", (event) => {
         calculadora.value += event.target.value;
     })
 }
-
+//Limpia el resultado y el value del input
 limpiar.addEventListener("click", () => {
   resultado = 0;
   calculadora.value = ""; 
 });
 
+//Click del igual que ejecutará la función del value y el resultado lo pondrá en el value del input
 igual.addEventListener("click", () => {
   try{
     resultado = eval(calculadora.value);
